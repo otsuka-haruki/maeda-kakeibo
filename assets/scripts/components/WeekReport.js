7b5febb433880a47ae16546d3c09964d2182152f
otsuka-haruki <for.business.in.general@gmail.com>
2021-01-03 18:38:34 +0900

    chart modified

--- assets/scripts/components/WeekReport.js
+++ assets/scripts/components/WeekReport.js
@@ -8,8 +8,31 @@ export class WeekReport {
 
   drawChart() {
     document.getElementById('tab__week').addEventListener('click', () => {
-      chartFunction.drawChartWeekBar();
-      chartFunction.drawChartWeekDoughnut();
+      const oldChartMonitors = document.querySelectorAll('#report__week .chartjs-size-monitor');
+      const oldCharts = document.querySelectorAll('#report__week canvas');
+      if (oldChartMonitors.length > 0) {
+        oldChartMonitors.forEach(element => {
+          element.remove();
+        });
+        oldCharts.forEach(element => {
+          element.remove();
+        })
+      }
+
+      (function() {
+        const container = document.querySelector('#report__week__this-week .card-content');
+        const template = document.getElementById('report__week__this-week__template-canvas-week-bar-out');
+        const clone = template.content.cloneNode(true);
+        container.append(clone);
+        chartFunction.drawChartWeekBar();
+      })();
+      (function() {
+        const container = document.querySelectorAll('#report__week__this-week .card-content')[1];
+        const template = document.getElementById('report__week__this-week__template-canvas-week-doughnut-out');
+        const clone = template.content.cloneNode(true);
+        container.append(clone);
+        chartFunction.drawChartWeekDoughnut();
+      })();
     });
   }
 }
